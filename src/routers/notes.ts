d40b35016133a3877dc3d245360cd23b7bdbb8d2
deadorchid <sadrezdev@gmail.com>
2026-03-04 22:08:17 +0300

    cors

--- src/routers/notes.ts
+++ src/routers/notes.ts
@@ -2,6 +2,8 @@ import Elysia from "elysia";
 import { notesTable } from "../db/schema";
 import { db } from "../db/db";
 
+import { cors } from "@elysiajs/cors";
+
 const router = new Elysia({ prefix: "/notes" });
 
 router.get("/", async () => {
@@ -10,5 +12,12 @@ router.get("/", async () => {
   if (results.length === 0) status(204, "Notes are empty");
   return results;
 });
+
+router.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend's URL
+    credentials: true, // Optional: if you need to support cookies
+  }),
+);
 
 export default router;
