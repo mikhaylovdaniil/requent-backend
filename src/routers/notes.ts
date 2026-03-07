6d779c596306ce86f846773c230d68ce128d59a8
deadorchid <sadrezdev@gmail.com>
2026-03-07 14:33:20 +0300

    complete and delete routes

--- src/routers/notes.ts
+++ src/routers/notes.ts
@@ -13,6 +13,43 @@ router.get("/", async () => {
   return results;
 });
 
+router.get("/:id", async ({ params: { id } }) => {
+  const result = await db
+    .select()
+    .from(notesTable)
+    .where(eq(notesTable.id, id));
+
+  if (result.length === 0) status(404, "Note not found");
+  return result;
+});
+
+router.put("/:id/complete", async ({ params: { id } }) => {
+  const note = await db.select().from(notesTable).where(eq(notesTable.id, id));
+
+  if (note.length === 0) status(404, "Note not found");
+
+  try {
+    const calculatedProgress = note[0].progress + 25;
+    await db
+      .update(notesTable)
+      .set({ progress: calculatedProgress })
+      .where(eq(notesTable.id, id)); // magic number
+    return { progress: calculatedProgress };
+  } catch (err) {
+    status(500, "Error occured while updating notes");
+  }
+});
+
+router.delete("/:id", async ({ params: { id } }) => {
+  try {
+    await db.delete(notesTable).where(eq(notesTable.id, id));
+    return { message: "Note Deleted" };
+  } catch (err) {
+    console.log(`Error while deleting note: ${err}`);
+    status(404, "Note not found");
+  }
+});
+
 router.use(
   cors({
     origin: "http://localhost:5173", // Replace with your frontend's URL
